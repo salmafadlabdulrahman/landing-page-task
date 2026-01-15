13e6facb9ba6b8781f951f4fbfe72831dffceee5
Salma Fadl <salmafadlabdulrahman@gmail.com>
2026-01-15 04:30:26 +0200

    optimize trial banner and footer

--- src/components/TrialBanner.jsx
+++ src/components/TrialBanner.jsx
@@ -2,19 +2,24 @@ import logo from "/assets/trial-logo.png";
 
 const TrialBanner = () => {
   return (
-    <div className="text-black-600 w-332.5 m-auto bg-white-300 rounded-[20px] px-8.75">
-      <div className="pt-31 pl-10 pb-23">
-        <img src={logo} alt="a yellow square" />
-        <p className="text-[46px] mt-5.25 font-semibold leading-[59.8px] tracking-[1.3px]">
-          Superpowers starting $9.99/month. <br /> Free for 7 days.
+    <section className="max-w-332.5 mx-auto bg-white-300 rounded-2xl px-9">
+      <div className="py-24 pl-10">
+        <img src={logo} alt="Setapp logo" />
+
+        <p className="mt-6 text-[46px] font-semibold leading-[1.3] tracking-wide text-black-600">
+          Superpowers starting $9.99/month.
+          <br />
+          Free for 7 days.
         </p>
 
-        <div className="mt-15 flex items-center gap-[24.5px]">
+        <div className="mt-15 flex items-center gap-6">
           <button className="btn-primary">Get started now</button>
-          <button className="btn-outline ">More about Setapp</button>
+          <button className="btn-outline text-black-600">
+            More about Setapp
+          </button>
         </div>
       </div>
-    </div>
+    </section>
   );
 };
 
